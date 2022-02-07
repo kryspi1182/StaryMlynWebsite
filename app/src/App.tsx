9a19d13a929956fafc1ec6e57851d596059c757f
Krystian Wysocki <krystian1182@wp.pl>
2022-02-07 16:33:45 +0100

    Create app

---
+++ app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ app/src/Home.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
